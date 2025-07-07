f7786951c7e800d95c11d00d1a911fed6d6c9d3f
Teriks <Teriks@users.noreply.github.com>
2025-07-06 19:10:05 -0500

    sync source version to tag

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="patchmatch-cython",
-    version="0.1.0",
+    version="0.1.3",
     author="Teriks",
     author_email="Teriks999@gmail.com",
     description="High-performance PatchMatch implementation for image inpainting using Cython",
